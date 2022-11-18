5583a72f4b3328d2ee74e928776b8d163e69507b
Clonky <clonky_cs@yahoo.de>
2022-11-18 01:17:25 +0100

    Add split code to improve organization

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+        menu_item: "./src/menu_item.js",
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
+        filename: "[name].bundle.js",
+        clean: true,
     },
     plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
     module: {
